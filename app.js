5582597ff99964808235bd134148dfb8758b81e6
juhyunham <judev0227@gmail.com>
2021-07-30 16:39:16 +0900

    Type Aliases

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 "use strict";
-var GenderType;
-(function (GenderType) {
-    GenderType["Male"] = "male";
-    GenderType["Female"] = "female";
-    GenderType["GenderNeutral"] = "genderNeutral";
-})(GenderType || (GenderType = {}));
-var student1 = {
-    studentID: 5555,
-    studentName: 'hamju5555',
-    gender: GenderType.Female,
-    subject: 'js5555',
-    courseCompleted: true
+var totalCost;
+var orderId;
+var calculateTotalCost = function (price, qty) {
 };
-//void는 아무것도 반환하지 않는다.
-function getStudentDetails(studeintID) {
-    return {
-        studentID: 1245,
-        studentName: 'hamju',
-        gender: 'male',
-        subject: 'js',
-        courseCompleted: false,
-        addComment: function () {
-            return 'haha';
-        }
-    };
-}
-function saveStudentDetails(student1) {
-}
-saveStudentDetails(student1);
+var findOrderId = function (customer, productId) {
+    return orderId;
+};
--- app.ts
+++ app.ts
@@ -1,45 +1,11 @@
-enum GenderType {
-	Male = 'male',
-	Female = 'female',
-	GenderNeutral = 'genderNeutral'
+type StrOrNum = number | string;
+let totalCost:number;
+let orderId: StrOrNum;
+
+const calculateTotalCost = (price: StrOrNum, qty: number):void  =>{
+
 }
 
-const student1 = {
-	studentID : 5555,
-	studentName: 'hamju5555',
-	gender: GenderType.Female,
-	subject: 'js5555',
-	courseCompleted: true
-}
-
-
-interface Student {
-	readonly studentID:number,
-	studentName:string,
-	age?:number,
-	gender:'male' | 'female' | 'genderNeutral',
-	subject:string,
-	courseCompleted:boolean,
-	addComment? (comment: string): string,
-	addComment?: (comment: string) => string
-}
-
-//void는 아무것도 반환하지 않는다.
-function getStudentDetails(studeintID:number): Student{
-	return {
-		studentID : 1245,
-		studentName: 'hamju',
-		gender: 'male',
-		subject: 'js',
-		courseCompleted: false,
-		addComment: () => {
-			return 'haha'
-		}
-	}
-}
-
-function saveStudentDetails (student1: Student):void {
-	
-}
-
-saveStudentDetails(student1)
+const findOrderId = (customer : {customerId: StrOrNum, nume: string}, productId: StrOrNum): StrOrNum => {
+	return orderId
+} 
