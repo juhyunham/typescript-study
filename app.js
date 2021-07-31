9a0739c76700587a17cf539b5cff609ef6d2075c
juhyunham <judev0227@gmail.com>
2021-07-31 14:24:16 +0900

    typescript - Class public, private

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 "use strict";
 var Employee = /** @class */ (function () {
-    function Employee(fullName, age, jobTitle, hourlyRate, workingHoursPerWeek) {
+    function Employee(_fullName, _age, _jobTitle, _hourlyRate, workingHoursPerWeek) {
         var _this = this;
-        this.printEmployeeDetails = function () {
-            console.log(_this._fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this.jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this.hourlyRate * _this.workingHoursPerWeek + "\uB2EC\uB7EC\uC774\uB2E4.");
-        };
-        this._fullName = fullName;
-        this.age = age;
-        this.jobTitle = jobTitle;
-        this.hourlyRate = hourlyRate;
+        this._fullName = _fullName;
+        this._age = _age;
+        this._jobTitle = _jobTitle;
+        this._hourlyRate = _hourlyRate;
         this.workingHoursPerWeek = workingHoursPerWeek;
+        this.printEmployeeDetails = function () {
+            console.log(_this._fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this._jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this._hourlyRate * _this.workingHoursPerWeek + "\uB2EC\uB7EC\uC774\uB2E4.");
+        };
     }
     Object.defineProperty(Employee.prototype, "fullName", {
         get: function () {
@@ -25,4 +25,5 @@ var Employee = /** @class */ (function () {
 }());
 var employee1 = new Employee("\uC8FC\uD604", 32, "\uC8FC\uB2C8\uC5B4\uAC1C\uBC1C\uC790", 40, 35);
 employee1.fullName = "\uC0AC\uB791";
+employee1.workingHoursPerWeek = 50;
 employee1.printEmployeeDetails();
