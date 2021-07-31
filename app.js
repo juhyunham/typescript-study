7a7d66b3816e333b8d70adb9556d13c02e9e5ea9
juhyunham <judev0227@gmail.com>
2021-07-31 14:21:20 +0900

    typescript - use class constructor

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 "use strict";
 var Employee = /** @class */ (function () {
-    function Employee() {
+    function Employee(fullName, age, jobTitle, hourlyRate, workingHoursPerWeek) {
         var _this = this;
         this.printEmployeeDetails = function () {
-            console.log(_this.fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this.jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this.hourlyRate * _this.workingHoursPerWeek + "\uB2EC\uB7EC\uC774\uB2E4.");
+            console.log(_this._fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this.jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this.hourlyRate * _this.workingHoursPerWeek + "\uB2EC\uB7EC\uC774\uB2E4.");
         };
+        this._fullName = fullName;
+        this.age = age;
+        this.jobTitle = jobTitle;
+        this.hourlyRate = hourlyRate;
+        this.workingHoursPerWeek = workingHoursPerWeek;
     }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (value) {
+            this._fullName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-var employee1 = new Employee();
-employee1.fullName = "\uBBFC\uC218";
-employee1.age = 28;
-employee1.jobTitle = "\uC8FC\uB2C8\uC5B4\uAC1C\uBC1C\uC790";
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 35;
+var employee1 = new Employee("\uC8FC\uD604", 32, "\uC8FC\uB2C8\uC5B4\uAC1C\uBC1C\uC790", 40, 35);
+employee1.fullName = "\uC0AC\uB791";
 employee1.printEmployeeDetails();
--- app.ts
+++ app.ts
@@ -1,21 +1,31 @@
 class Employee {
-	fullName: string
+	private _fullName: string
 	age: number
 	jobTitle: string
 	hourlyRate: number 
 	workingHoursPerWeek: number 
 
+	constructor (fullName:string, age: number, jobTitle: string, hourlyRate: number , workingHoursPerWeek: number ) {
+		this._fullName = fullName
+		this.age = age 
+		this.jobTitle = jobTitle
+		this.hourlyRate = hourlyRate 
+		this.workingHoursPerWeek = workingHoursPerWeek 
+	}
+
+	get fullName () {
+		return this._fullName;
+	}
+
+	set fullName (value: string) {
+		this._fullName = value
+	}
+
 	printEmployeeDetails = ():void => {
-		console.log(`${this.fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek}달러이다.`)
+		console.log(`${this._fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek}달러이다.`)
 	}
 }
 
-let employee1 = new Employee();
-
-employee1.fullName = `민수`
-employee1.age = 28
-employee1.jobTitle = `주니어개발자`
-employee1.hourlyRate = 40
-employee1.workingHoursPerWeek = 35
-
+let employee1 = new Employee(`주현`, 32, `주니어개발자`, 40, 35);
+employee1.fullName = `사랑`
 employee1.printEmployeeDetails();
